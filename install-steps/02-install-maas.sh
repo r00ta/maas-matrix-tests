3632f4a67b18769e2f1b138af1f74aab8328d4b6
Jacopo Rota <jacopo.r00ta@gmail.com>
2024-04-09 13:30:56 +0200

    Update 02-install-maas.sh

--- install-steps/02-install-maas.sh
+++ install-steps/02-install-maas.sh
@@ -7,10 +7,8 @@ echo "Installing MAAS.."
 if [ -z "$MAAS_VERSION" ]; then
 	sudo snap install --channel=latest/edge maas-test-db
 	original_dir="$(pwd)"
-	git clone https://git.launchpad.net/~troyanov/maas --recurse-submodules --remote-submodules /tmp/maas/code
+	git clone https://git.launchpad.net/~maas/maas --recurse-submodules --remote-submodules /tmp/maas/code
 	cd /tmp/maas/code
-	git fetch
-	git checkout agent-wf-vlan-reconfigure
 	make snap-tree
 	sudo snap try dev-snap/tree
 	utilities/connect-snap-interfaces
